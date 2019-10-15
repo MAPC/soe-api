ae14d0dc0d148eb15bfe0bad2d80682a3f858587
Brad Smith <brad@smithwebtek.com>
2019-10-15 14:28:34 -0400

    Add cors handling in main controller

--- app/controllers/api/main_controller.rb
+++ app/controllers/api/main_controller.rb
@@ -1,4 +1,7 @@
 class Api::MainController < ApplicationController
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
+
   require_relative '../../data/diversity/diversity1'
   include Diversity1
 
@@ -181,6 +184,28 @@ class Api::MainController < ApplicationController
 
   def home_ownership2
     render json: HOME_OWNERSHIP2
+  end
+
+  # For all responses in this controller, return the CORS access control headers.
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  # If this is a preflight OPTIONS request, then short-circuit the
+  # request, return only the necessary headers and return an empty
+  # text/plain.
+
+  def cors_preflight_check
+    if request.method == :options
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
+      headers['Access-Control-Max-Age'] = '1728000'
+      render :text => '', :content_type => 'text/plain'
+    end
   end
 
 end
